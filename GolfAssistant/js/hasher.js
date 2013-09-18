79eebcdf066aff7b311f8c40b64cbf5cab50cf52
Tobias <tobias.p.lund@gmail.com>
2013-09-18 15:43:33 +0200

    Central storage

---
+++ GolfAssistant/js/hasher.js
@@ -0,0 +1,67 @@
+﻿(function (window, undefined) {
+
+    var
+
+    callbackQue = [],
+
+    hashTriggers = document.querySelectorAll('[data-hasher-href]'),
+
+    tracker = {
+        oldUrl: undefined,
+        oldHash: undefined,
+    },
+
+    hasher = {
+
+        hashchange: function (callback) {
+            callbackQue.push(callback);
+            callback.call(null, { oldUrl: undefined, newUrl: tracker.oldUrl, oldHash: undefined, newHash: tracker.oldHash });
+        },
+
+        gotoHash: function (hash) {
+            window.location.href = window.location.href.split('#')[0] + '#' + hash;
+        }
+    },
+    
+    hashchangeEventHandler = function (e) {
+        var
+            oldUrl = e.oldURL || tracker.oldUrl,
+            newUrl = e.newURL || window.location.href,
+            oldHash = oldUrl.split('#')[1],
+            newHash = newUrl.split('#')[1];
+
+        tracker.oldUrl = newUrl;
+        tracker.oldHash = newHash;
+
+        for (var i = 0; i < callbackQue.length; i++) {
+            callbackQue[i].call(null, { oldUrl: oldUrl, newUrl: newUrl, oldHash: oldHash, newHash: newHash });
+        };
+    },
+
+    hashTriggerClicked = function (hashTrigger) {
+        hasher.gotoHash(hashTrigger.getAttribute('data-hasher-href'));
+    },
+
+    initHasher = function () {
+        var initialHash = window.location.href.split('#')[1];
+        if (initialHash) {
+            tracker.oldUrl = window.location.href;
+            tracker.oldHash = initialHash;
+        }
+    };
+
+    window.addEventListener("hashchange", hashchangeEventHandler, false);
+
+    for (var i = 0; i < hashTriggers.length; i++) {
+        (function (i) {
+            hashTriggers[i].addEventListener('click', function () {
+                hashTriggerClicked(hashTriggers[i]);
+            }, null)
+        })(i);
+    };
+
+    initHasher();
+
+    window.hasher = hasher;
+
+})(window);
--- GolfAssistant/js/main.js
+++ GolfAssistant/js/main.js
@@ -5,9 +5,9 @@ window.addEventListener('load', function() {
 function viewModel() {
     var self = this;
 
-	this.contentView = {
-		viewName: ko.observable('round'),
-		viewIndex: ko.observable(0)		
+	this.views = {
+		contentView: ko.observable('round'),
+		clubTabView: ko.observable('nearest-clubs')
 	}
 
 	this.account = {
@@ -20,7 +20,14 @@ function viewModel() {
 	this.round = {
 	    started: ko.observable(false),
 	    start: function () {
-	        self.contentView.viewName('round-select-club');
+	        hasher.gotoHash('round-select-club');
+	        self.round.started(true);
+	    }
+	}
+
+	this.commonFn = {
+	    changeHash: function (hash, viewModel, event) {
+	        hasher.gotoHash(hash);
 	    }
 	}
 }
@@ -38,5 +45,9 @@ var geo = new GEO();
 ajax('mock/Courses.json', function (data) {
     courses = JSON.parse(data);
     console.log(geo.distance(57.779614, 14.164979, courses[1].Lat, courses[1].Lon));
+});
+
+hasher.hashchange(function (hashData) {
+    vm.views.contentView(hashData.newHash || 'home');
 });
 
