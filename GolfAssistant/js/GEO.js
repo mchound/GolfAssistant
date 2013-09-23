e6191b2d1bccd8332836557a89e8fe8f9638e57e
Tobias Lund <tobias.p.lund@gmail.com>
2013-09-23 12:45:45 +0200

    Central storage

--- GolfAssistant/js/GEO.js
+++ GolfAssistant/js/GEO.js
@@ -1,17 +1,87 @@
-﻿function GEO() {
-
+﻿var GEO = function () {
+    this.lat = undefined;
+    this.lon = undefined;
+    this.position = undefined;
+    this.watchId = undefined;
+    this.status = 'stopped';
+    this.subscribers = [];
+    this.waiters = [];
+    this.options = {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000
+    };
 };
 
 GEO.prototype.distance = function (lat1, lon1, lat2, lon2) {
-    var R = 6371; // km
-    var dLat = Math.toRad(lat2 - lat1);
-    var dLon = Math.toRad(lon2 - lon1);
-    var lat1 = Math.toRad(lat1);
-    var lat2 = Math.toRad(lat2);
+    var
+    R = 6371, // km
+    dLat = Math.toRad(lat2 - lat1),
+    dLon = Math.toRad(lon2 - lon1),
+    lat1 = Math.toRad(lat1),
+    lat2 = Math.toRad(lat2),
 
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
+    a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2),
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)),
+    d = R * c;
     return d*1000;
+};
+
+GEO.prototype.start = function (callback) {
+    
+    if ("geolocation" in navigator) {
+        var self = this;
+        this.status = 'pending';
+        this.watchId = navigator.geolocation.watchPosition(function (position) {
+            self.status = 'locked';
+            if (callback) callback.call(self, position);
+            self.update.call(self, position);
+        }, this.error, this.options)
+    }
+    else throw new TypeError("Geolocation isn't supported in browser");
+};
+
+GEO.prototype.stop = function () {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.clearWatch(this.watchId);
+        this.status = 'stopped';
+    }
+};
+
+GEO.prototype.update = function (position) {
+    this.lat = position.coords.latitude;
+    this.lon = position.coords.longitude;
+    this.position = position;
+
+    for (var i = 0; i < this.subscribers.length; i++) {
+        this.subscribers[i].call(this, position);        
+    };
+
+    for (var i = 0; i < this.waiters.length; i++) {
+        this.waiters[i].call(this, position);
+    };
+
+    this.waiters = [];
+};
+
+GEO.prototype.error = function (error) {
+    console.log('ERROR(' + error.code + '): ' + error.message);
+};
+
+GEO.prototype.subscribe = function (callback) {
+    if (typeof callback !== 'function') return;
+
+    this.subscribers.push(callback);
+};
+
+GEO.prototype.getPosition = function (callback) {
+    if (this.status === 'stopped') {
+        this.start(callback);
+    }
+    else if (this.status === 'pending') {
+        this.waiters(callback);
+    }
+    else {
+        callback.call(this, this.position);
+    }
 };
--- GolfAssistant/js/main.js
+++ GolfAssistant/js/main.js
@@ -5,56 +5,121 @@ window.addEventListener('load', function() {
 function viewModel() {
     var self = this;
 
-	this.views = {
-		contentView: ko.observable('round'),
-		clubTabView: ko.observable('nearest-clubs')
-	}
+    this.views = {
+        contentView: ko.observable('round'),
+        clubTabView: ko.observable('nearest-clubs')
+    };
 
-	this.account = {
-		name: ko.observable('Tobias Lund'),
-		email: ko.observable('tobias.lund@hotmail.com'),
-		handicap: ko.observable(18.9),
-        gender: ko.observable(1)
-	}
+    this.account = {
+        name: ko.observable('Tobias Lund'),
+        email: ko.observable('tobias.lund@hotmail.com'),
+        handicap: ko.observable(18.9),
+        gender: ko.observable("1")
+    };
 
-	this.round = {
-	    started: ko.observable(false),
-	    start: function () {
-	        hasher.gotoHash('round-select-club');
-	        self.round.started(true);
-	    }
-	}
+    this.round = {
+        started: ko.observable(false),
+        club: ko.observable({ Data: { Courses: [] }}),
+        course: ko.observable({}),
+        start: function () {
+            self.round.started(true);
+            hasher.gotoHash('round-select-club');
+        },
+        clubSelect: function (club) {
+            self.round.club(club);
+            hasher.gotoHash('round-select-course');
+        },
+        courseSelect: function (course) {
+            self.round.course(course);
+            hasher.gotoHash('round-select-tee');
+        }
+    };
 
-	this.commonFn = {
-	    changeHash: function (hash, viewModel, event) {
-	        hasher.gotoHash(hash);
-	    }
-	}
+    this.commonFn = {
+        changeHash: function (hash, viewModel, event) {
+            hasher.gotoHash(hash);
+        },
 
-	this.store = {
-	    courses: ko.observableArray([]),
-	    clubSearchQuery: ko.observable('')	    
-	},
+        strokes: function (tee, par) {
+            var
+            hcp = self.account.handicap();
+
+            return Math.floor(hcp * (tee.SlopeValue / 113) + (tee.CourseRating - par));
+        }
+    };
+
+    this.store = {
+        courses: ko.observableArray([]),
+        clubSearchQuery: ko.observable('')
+    };
+
+    this.geo = {
+        lat: ko.observable(),
+        lon: ko.observable()
+    };
 
     this.search = {
-        filteredCourses: ko.computed(function () {
+        filteredClubs: ko.computed(function () {
             var searchQuery = self.store.clubSearchQuery();
             return ko.utils.arrayFilter(this.courses(), function (course) {
                 return course.Name.toLowerCase().indexOf(searchQuery) != -1;
             });
-        }, self.store)
-    }
-}
- 
- var vm = new viewModel();
- ko.applyBindings(vm);
+        }, self.store),
 
+        nearest: ko.computed(function () {
+            var
+            lat = self.geo.lat();
+            lon = self.geo.lon();
+            return this.courses.sort(function (left, right) {
+                var
+                leftDistance = geo.distance(left.MapLat, left.MapLng, lat, lon),
+                rightDistance = geo.distance(right.MapLat, right.MapLng, lat, lon);
+                left.distance = Math.round(leftDistance / 100) / 10;
+                right.distance = Math.round(rightDistance / 100) / 10;
+                return leftDistance == rightDistance ? 0 : leftDistance > rightDistance ? 1 : -1;
+            });
+        }, self.store).extend({ throttle: 500 }),
+
+        tees: ko.computed(function () {
+            var
+            course = self.round.course();
+            if (!course.Loop) return [];
+
+            return ko.utils.arrayFilter(course.Loop.Slopes, function (slope) {
+                slope.strokes = self.commonFn.strokes(slope, self.round.course().Par);
+                return slope.Gender === parseInt(self.account.gender());
+            }).sort(function (left, right) {
+                return left.strokes === right.strokes ? 0 : left.strokes > right.strokes ? 1 : -1;
+            });
+        })
+    };
+
+    this.views.contentView.subscribe(function (newValue) {
+        if (['round-select-club', 'round-select-course', 'round-select-tee'].indexOf(newValue) !== -1 && !self.round.started())
+            hasher.gotoHash('round');
+        
+        if (['round-select-club', 'round-select-course'].indexOf(newValue) !== -1 && self.round.started())
+            geo.start();
+        else
+            geo.stop();
+    });
+
+};
+ 
 var mainViews = ['home', 'settings', 'round', 'stats'];
 var roundViews = ['']
 
 
 var courses;
+
 var geo = new GEO();
+geo.subscribe(function (position) {
+    vm.geo.lat(position.coords.latitude);
+    vm.geo.lon(position.coords.longitude);
+});
+
+var vm = new viewModel();
+ko.applyBindings(vm);
 
 ajax('mock/Courses.json', function (data) {
     vm.store.courses(JSON.parse(data));
